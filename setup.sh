b0e6cb389f3359d1bd761bb162316d2635c882aa
William Zhang <willzhang05@gmail.com>
2018-01-12 09:22:03 -0500

    add uncommited changes

--- setup.sh
+++ setup.sh
@@ -2,9 +2,14 @@
 cp .zshrc ~/.zshrc
 
 cp .vimrc ~/.vimrc
-#mkdir -p ~/.vim/bundle
-#git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
-#git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+mkdir -p ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 cd ~
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-sed -e "s/THEME=\"robbyrussell/THEME=\"gentoo/g" ~/.zshrc
+if [ ! -d ~/.oh-myzsh ]; then
+    wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+    chmod +x ~/install.sh
+    ~/install.sh &
+fi
+sed -e "s/THEME=\"robbyrussell/THEME=\"gentoo/g" ~/.zshrc > ~/.zshrc
+rm -f install.sh*
