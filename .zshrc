1f860a760ebb3543ea2827f404aa6b8fdc2616a4
William Zhang <willzhang05@gmail.com>
2018-01-12 09:47:59 -0500

    fix setup.sh

--- .zshrc
+++ .zshrc
@@ -1,8 +1,8 @@
 # If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:/usr/local/bin:$PATH
+ export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-  export ZSH=/home/williamzhang/.oh-my-zsh
+  export ZSH=$HOME/.oh-my-zsh
 
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
@@ -90,4 +90,4 @@ alias vi='vim'
 export VISUAL=vim
 export EDITOR="$VISUAL"
 #export WORKON_HOME=~/envs
-#source /usr/bin/activate.sh
+source /usr/bin/activate.sh
--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-cp .zshrc ~/.zshrc
 
 cp .vimrc ~/.vimrc
-mkdir -p ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-cd ~
-if [ ! -d ~/.oh-myzsh ]; then
-    wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-    chmod +x ~/install.sh
-    ~/install.sh &
+if [ ! -d ~/.vim ]; then
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+    git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 fi
-sed -e "s/THEME=\"robbyrussell/THEME=\"gentoo/g" ~/.zshrc > ~/.zshrc
+if [ ! -d ~/.oh-my-zsh ]; then
+    git clone https://github.com/robbyrussell/oh-my-zsh 
+    chmod +x oh-my-zsh/tools/install.sh
+    oh-my-zsh/tools/install.sh &
+fi
+cp .zshrc ~/.zshrc
+sed -i "s/THEME=\"robbyrussell/THEME=\"gentoo/g" ~/.zshrc
+rm -rf oh-my-zsh
 rm -f install.sh*
